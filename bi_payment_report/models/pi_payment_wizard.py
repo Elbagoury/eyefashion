7bec2d6f8cc6097d452bf5085ffc23e1e9e13059
FatmaDaify <fdaify@iskydev.com>
2018-08-04 22:48:08 +0200

    [UPD] payment report to add domains and print dates

--- bi_payment_report/models/pi_payment_wizard.py
+++ bi_payment_report/models/pi_payment_wizard.py
@@ -9,22 +9,22 @@ class PaymentReport(models.AbstractModel):
     @api.model
     def render_html(self, docids, data):
         grouped_dict = {}
-        print ("hereeeeeeeeee")
+        date_from = data['date_from']
+        date_to = data['date_to']
         payment_objects = self.env['account.payment'].browse(data['ids'])
-        # group payment {'date_team_journal': {'name':grouped_name ,'date':payment_date, 'team':sale_team_id,'journal':journal_id,'records':[ gruoped records]},....}
+        # group payment {'date': {'date':payment_date, 'team':sale_team_id,'journal':journal_id,'records':[ gruoped records]},....}
         for payment in payment_objects:
             if payment.sale_team_id:
-                grouped_name = payment.payment_date + " - " + payment.sale_team_id.name + " - " + payment.journal_id.name
-                if grouped_name in grouped_dict:
-                    grouped_dict[grouped_name]['records'].append(payment)
+                date = payment.payment_date
+                if date in grouped_dict:
+                    print("yes")
                 else:
-                    grouped_dict[grouped_name] = {'name': grouped_name, 'date': payment.payment_date,
-                                                  'team': payment.sale_team_id.name,
-                                                  'journal': payment.journal_id.name,
-                                                  'records': [payment]}
+                    grouped_dict[date] = {'date': payment.payment_date,
+                                          }
         docs = {
             'doc_model': 'account.payment',
             'docs': grouped_dict,
+            'dates': [date_from, date_to]
 
         }
         return self.env['report'].render('bi_payment_report.account_payment_template', docs)
@@ -36,14 +36,15 @@ class PaymentReportWizard(models.TransientModel):
     date_from = fields.Date(string='From', required=True, default=fields.Date.context_today)
     date_to = fields.Date(string='To', required=True, default=fields.Date.context_today)
     select_all_records = fields.Boolean(string='Select All')
-    journal_ids = fields.Many2many('account.journal', string='Journals', required=True)
+    journal_ids = fields.Many2many('account.journal', string='Journals', required=True,
+                                   domain=[('type', 'in', ('cash', 'bank'))])
     team_ids = fields.Many2many('crm.team', string='Sales Team', required=True)
 
     @api.onchange('select_all_records')
     def get_journal_and_teams(self):
         for rec in self:
             if rec.select_all_records:
-                rec.journal_ids = self.env['account.journal'].search([]).ids
+                rec.journal_ids = self.env['account.journal'].search([('type', 'in', ('cash', 'bank'))]).ids
                 rec.team_ids = self.env['crm.team'].search([]).ids
 
     @api.multi
@@ -53,11 +54,13 @@ class PaymentReportWizard(models.TransientModel):
                 raise ValidationError(_('Date from must be less than date to'))
 
         search_domain = [('payment_date', '>=', rec.date_from), ('payment_date', '<=', rec.date_to),
+                         ('payment_type', '=', 'inbound'),
                          ('sale_team_id', 'in', rec.team_ids.ids), ('journal_id', 'in', rec.journal_ids.ids)]
         payment_objects = self.env['account.payment'].search(search_domain)
 
         data = {
             'ids': payment_objects.ids,
-
+            'date_from': rec.date_from,
+            'date_to': rec.date_to,
         }
         return self.env['report'].get_action(self, 'bi_payment_report.account_payment_template', data=data)
